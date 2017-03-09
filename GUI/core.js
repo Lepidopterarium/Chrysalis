f34ebd2e320eefb33b5c8410578be07e92071bb8
TheBaronHimself <accounts@simonclaudius.com>
2017-03-09 14:40:27 +0100

    Implement mouse event listener

--- GUI/core.js
+++ GUI/core.js
@@ -1,34 +1,39 @@
 var c = 0;
 var r = 0;
+var keyCoord = "R0C0";
+
 
 document.getElementById("up").addEventListener("click", function() {
-    keySwitch(r, c, 'off');
+    keySwitch(keyCoord, "off");
     r = decrementVar(r, 3);
+    keyCoord = createCoordinates(r, c);
     document.getElementById("debug_r").innerHTML = r;
-    keySwitch(r, c, 'on');
+    keySwitch(keyCoord, "on", "grey");
 });
 
 document.getElementById("left").addEventListener("click", function() {
-    keySwitch(r, c, 'off');
-    c = decrementVar(c, 15);
+    keySwitch(keyCoord, "off");
+    c = decrementVar(c, 15); 
+    keyCoord = createCoordinates(r, c);
     document.getElementById("debug_c").innerHTML = c;
-    keySwitch(r, c, 'on');
+    keySwitch(keyCoord, "on", "grey");
 });
 
 document.getElementById("right").addEventListener("click", function() {
-    keySwitch(r, c, 'off');
+    keySwitch(keyCoord, "off");
     c = incrementVar(c, 15);
+    keyCoord = createCoordinates(r, c);
     document.getElementById("debug_c").innerHTML = c;
-    keySwitch(r, c, 'on');
+    keySwitch(keyCoord, "on", "grey");
 });
 
 document.getElementById("down").addEventListener("click", function() {
-    keySwitch(r, c, 'off');
-    r = incrementVar(r, 3); 
+    keySwitch(keyCoord, "off");
+    r = incrementVar(r, 3);
+    keyCoord = createCoordinates(r, c);
     document.getElementById("debug_r").innerHTML = r;
-    keySwitch(r, c, 'on');
+    keySwitch(keyCoord, "on", "grey");
 });
-
 
 
 function decrementVar(i, limit) {
@@ -51,31 +56,56 @@ function incrementVar(i, limit) {
 };
 
 
-function keySwitch(r, c, keyState) {
-    // Get the Object by ID
+function createCoordinates(r, c) {
 
-
+    // Create coordinate string
     var string_array = ["R", r, "C", c];
+    var keyCoord = string_array.join("");
+    return keyCoord;
+};
 
-    var keyCode = string_array.join("");
 
+function keySwitch(keyCoord, keyState, colour) {
+    // Get the SVG document
     var a = document.getElementById("svgObject");
-    // Get the SVG document inside the Object tag
     var svgDoc = a.contentDocument;
-    // Get one of the SVG items by ID;
 
-
-    var svgFill = svgDoc.getElementById(keyCode.concat("_F"));
-    var svgStroke = svgDoc.getElementById(keyCode.concat("_S"));
-    // Set the colour to something else
+    // Get SVG elements for fill and stroke of the current key
+    var svgFill = svgDoc.getElementById(keyCoord.concat("_F"));
+    var svgStroke = svgDoc.getElementById(keyCoord.concat("_S"));
 
     if (keyState == 'on') {
-    svgFill.setAttribute("fill", "gray");
-    svgStroke.setAttribute("stroke-width", "5");
-    }
-    else {
+        // Turn key on
+        svgFill.setAttribute("fill", colour);
+        svgStroke.setAttribute("stroke-width", "5");
+    } else {
+        // Turn key off
         svgFill.setAttribute("fill", "#FFF");
-        svgStroke.setAttribute("stroke-width", "0.8");        
+        svgStroke.setAttribute("stroke-width", "0.8");
     };
 };
 
+
+function showEvtData(evt) {
+
+    // Get event information
+    var evtType = evt.type;
+    var evtID = evt.target.getAttributeNS(null, "id");
+
+    // Print event information
+    document.getElementById("debug_event_type").innerHTML = evtType;
+    document.getElementById("debug_event_element_ID").innerHTML = evtID;
+
+    // Remove element type suffix
+    var keyCoord = evtID.split("_")[0];
+
+
+    // Switch key states
+    if (evtType == "mouseover") {
+        keySwitch(keyCoord, "on", "grey");
+    } else if (evtType == "mouseout") {
+        keySwitch(keyCoord, "off");
+    } else if (evtType == "click") {
+        keySwitch(keyCoord, "on", "red");
+    };
+};
