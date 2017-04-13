974cf5a2cf8b0033860ab5758aa4cb54030e567c
Gergely Nagy <algernon@madhouse-project.org>
2017-04-13 18:45:51 +0200

    Start redoing the application
    Not going to write a long commit message here, sorry. It's like starting fresh!
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- renderer.js
+++ renderer.js
@@ -1,40 +1,16 @@
 const serialport = require('serialport')
 const marked = require('marked');
 
+var Focus = require("./lib/Focus/index")
+
 var port = new serialport ("/dev/ttyACM0")
-
-function appendBox(object, text) {
-    var box = document.createElement ("pre")
-    var node = document.createTextNode (text)
-
-    box.appendChild (node)
-    object.appendChild (box)
-}
-
-function processResult (res) {
-    return res.substring(0, res.length - 4)
-}
-
-function sendCommand () {
-    var cmd = document.getElementById("command").value
-    talk (cmd)
-    document.getElementById("command").value = ""
-    return false
-}
-
-function talk(command) {
-    port.write (command + "\n", function () {
-        port.drain (function (e) {
-            window.setTimeout (function () {
-                var o = document.getElementById('output')
-                appendBox (o, processResult (port.read().toString()))
-            }, 100)
-        })
-    })
-}
 
 port.on('open', function() {
     port.flush()
     port.drain()
     port.read() // dummy read, no idea why it is needed...
 });
+
+Focus.commands.version(port).then((version) => {
+    $("#device").html ("<pre>" + version.device.manufacturer + "/" + version.device.product + "</pre>")
+})
