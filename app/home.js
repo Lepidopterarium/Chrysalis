42b6e707d4feb70202571be7ad81357f9c6fdb66
Gergely Nagy <algernon@madhouse-project.org>
2017-04-14 20:13:06 +0200

    Pluggable device detection
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- app/home.js
+++ app/home.js
@@ -1,23 +1,15 @@
 window.Chrysalis = require("../lib/Chrysalis/index")
 
-Chrysalis.device.isSupported = function (port) {
-    if ((port.manufacturer == "Keyboardio") ||
-        (port.manufacturer == "Shortcut"))
-        return true
-    return false
-}
+Chrysalis.hardware.loadAll ()
+Chrysalis.device.detect ()
 
-Chrysalis.device.detect ().catch (() => {
-    console.log ("no devices found")
-})
-
-Chrysalis.once ("device-detected", (device) => {
+Chrysalis.on ("device-detected", (device) => {
     Chrysalis.device.open (device.comName)
 })
 
-Chrysalis.once ("device-ready", () => {
+Chrysalis.on ("device-ready", () => {
     Chrysalis.commands.version().then((version) => {
-        Chrysalis.hardware.load(version)
-        $("#device").html ("<a href='#' class='btn btn-outline-secondary'><img src='../lib/" + Chrysalis.device.meta.logo + "'></a>")
+        device = Chrysalis.hardware.select (version)
+        $("#device").append ("<a href='#' class='btn btn-outline-secondary'><img src='../lib/" + device.assets.logo + "'></a>")
     })
 })
