5f356c197fd84a098c7eb8654f49dfad6d722f39
Gergely Nagy <algernon@madhouse-project.org>
2017-04-14 20:32:06 +0200

    Improved device detection pluggability
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- app/home.js
+++ app/home.js
@@ -4,12 +4,16 @@ Chrysalis.hardware.loadAll ()
 Chrysalis.device.detect ()
 
 Chrysalis.on ("device-detected", (device) => {
-    Chrysalis.device.open (device.comName)
+    var hw = Chrysalis.hardware.find (device)
+    $("#device").append ("<a href='#' class='btn btn-outline-secondary'><img src='../lib/" + hw.assets.logo + "'></a>")
 })
 
+
+/*
 Chrysalis.on ("device-ready", () => {
     Chrysalis.commands.version().then((version) => {
         device = Chrysalis.hardware.select (version)
         $("#device").append ("<a href='#' class='btn btn-outline-secondary'><img src='../lib/" + device.assets.logo + "'></a>")
     })
 })
+*/
