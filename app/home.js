e89c728649f0814646451c8cdd0ba38107b0bdcd
Gergely Nagy <algernon@madhouse-project.org>
2017-04-15 12:59:11 +0200

    Open the selected device on click
    Fixes #5.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- app/home.js
+++ app/home.js
@@ -9,13 +9,13 @@ Chrysalis.on ("device-detected", (device) => {
                          "<div class='card-block'>" +
                          "<div class='card-text'><img class='' src='../lib/" + hw.assets.logo + "' alt='...'></div>" +
                          "</div><div class='card-footer text-muted'>" +
-                         "<button type='button' class='btn btn-primary chrysalis-device-select' data-device='" + hw.meta.id + "'>Select</button>" +
+                         "<button type='button' class='btn btn-primary chrysalis-device-select' data-device='" + device.comName + "'>Select</button>" +
                          "</div></div></div>")
 })
 
 $(document).on ("click", ".chrysalis-device-select", (event) => {
-    console.log($(event.currentTarget).data('device'))
-    //Chrysalis.device.open ("/dev/ttyACM0")
+    var device = $(event.currentTarget).data('device')
+    Chrysalis.device.open (device)
 })
 
 Chrysalis.on ("device-ready", () => {
