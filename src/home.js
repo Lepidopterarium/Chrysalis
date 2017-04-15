2c3a3896347b6f985b26c76679b59e229bf7cb64
Gergely Nagy <algernon@madhouse-project.org>
2017-04-15 13:57:25 +0200

    Start migrating UI updates to React
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- src/home.js
+++ src/home.js
@@ -1,16 +1,27 @@
-window.Chrysalis = require("../lib/Chrysalis/index")
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 Chrysalis.hardware.loadAll ()
 Chrysalis.device.detect ()
 
 Chrysalis.on ("device-detected", (device) => {
     var hw = Chrysalis.hardware.find (device)
-    $("#device").append ("<div class='col-sm-6'><div class='card'>" +
-                         "<div class='card-block'>" +
-                         "<div class='card-text'><img class='' src='../lib/" + hw.assets.logo + "' alt='...'></div>" +
-                         "</div><div class='card-footer text-muted'>" +
-                         "<button type='button' class='btn btn-primary chrysalis-device-select' data-device='" + device.comName + "'>Select</button>" +
-                         "</div></div></div>")
+    ReactDOM.render(
+            <div className="col-sm-6">
+              <div className="card">
+                <div className="card-block">
+                  <div className="card-text">
+                    <img src={ "../lib/" + hw.assets.logo } alt='...' />
+                  </div>
+                </div>
+                <div className="card-footer text-muted">
+                  <button type="button" className="btn btn-primary chrysalis-device-select"
+                          data-device={ device.comName }>Select</button>
+                </div>
+              </div>
+            </div>,
+        document.getElementById('device')
+    )
 })
 
 $(document).on ("click", ".chrysalis-device-select", (event) => {
