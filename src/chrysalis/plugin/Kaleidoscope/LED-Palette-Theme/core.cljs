126279c9b3bac5d4b0730381734647036cbab2d3
James N. V. Cash <james.nvc@gmail.com>
2017-11-03 14:13:06 -0400

    Display palette in repl

--- src/chrysalis/plugin/Kaleidoscope/LED-Palette-Theme/core.cljs
+++ src/chrysalis/plugin/Kaleidoscope/LED-Palette-Theme/core.cljs
@@ -17,7 +17,8 @@
 (ns chrysalis.plugin.Kaleidoscope.LED-Palette-Theme.core
   (:require [chrysalis.command.post-process :as post-process]
             [chrysalis.plugin.page.repl.core :refer [display repl-wrap]]
-            [clojure.string :as string]))
+            [clojure.string :as string]
+            [chrysalis.ui :as ui]))
 
 (defmethod post-process/format [:led.palette] [_ text]
   (->> (string/split text #" ")
@@ -25,7 +26,19 @@
        (partition 3)
        (mapv vec)))
 
-;; TODO: nice display for palette in repl
-#_(defmethod display :palette [_ req result device index]
-  (repl-wrap req index device result
-             []))
+(defmethod display :palette [_ req result device index]
+  (repl-wrap
+    req index device result
+    (into [:pre]
+          (map-indexed
+            (fn [i [r g b]]
+              [:span.badge
+               {:style {:background-color (str "rgb(" r "," g "," b ")")
+                        :color (str "rgb(" (bit-xor 0xff r) ","
+                                    (bit-xor 0xff g) ","
+                                    (bit-xor 0xff b) ")")
+                        :display "inline-block"
+                        :border "1px solid black"
+                        :margin "1em"}}
+               i " " (ui/color->hex [r g b])]))
+          (post-process/format :led.palette result))))
